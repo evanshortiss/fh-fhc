412818da5a8f22dc4dd38db3cceb6c6123facd46
Evan Shortiss <evanshortiss@gmail.com>
2017-01-25 16:16:53 -0600

    add ability to update nested service properties

--- test/unit/fh3/services/test_services.js
+++ test/unit/fh3/services/test_services.js
@@ -4,6 +4,9 @@ var servicesCmd = require('cmd/fh3/services');
 var serviceFixtures = require('test/fixtures/fixture_service');
 var dataSourceFixtures = require('test/fixtures/appforms/fixture_data_source');
 
+var sinon = require('sinon');
+var proxyquire = require('proxyquire');
+
 var mockService = serviceFixtures.get();
 var mockDataSource = dataSourceFixtures.get();
 
@@ -14,6 +17,69 @@ module.exports = {
       assert.ok(!err, "Expected No Error");
       //Should be an array of data sources.
       assert.equal(mockDataSource._id, serviceDataSources[0]._id);
+
+      done();
+    });
+  },
+
+  'should set a nested title property on apps[0]': function (done) {
+    var newTitle = 'shadowman\'s awesome app';
+
+    var mockGuid = 'someserviceguid'
+      , mockUrl = 'dummy.feedhenry.com';
+
+    var commonMocks = {
+      doGetApiCall: sinon.stub()
+    };
+
+    var fhreqMocks = {
+      getFeedHenryUrl: sinon.stub().returns(mockUrl),
+      PUT: sinon.stub()
+    };
+
+    var STUB_NAMES = {
+      COMMON: '../../common',
+      REQUEST: '../../utils/request'
+    };
+
+    var stubs = {};
+    stubs[STUB_NAMES.COMMON] = commonMocks;
+    stubs[STUB_NAMES.REQUEST] = fhreqMocks;
+
+    // readService calls this so wire it to return dummy data
+    commonMocks.doGetApiCall.yields(null, serviceFixtures.get());
+
+    // PUT call response format - cb(err, remoteData, raw, response)
+    fhreqMocks.PUT.yields(null, {}, '{}', { statusCode: 200 })
+
+    var service = proxyquire('cmd/fh3/services', stubs);
+
+    service({
+      _: [
+        'update',
+        mockGuid,
+        'apps[0].title',
+        newTitle
+      ]
+    }, function (err) {
+      assert.ok(!err);
+      assert.ok(
+        stubs[STUB_NAMES.COMMON].doGetApiCall.calledOnce,
+        'common should be called to get existing app info'
+      );
+
+      // Generate expected data for PUT
+      var expectedPutData = serviceFixtures.get();
+      expectedPutData.apps[0].title = newTitle;
+
+      assert.ok(
+        stubs[STUB_NAMES.REQUEST].PUT.calledWith(
+          mockUrl,
+          'box/api/connectors/' + mockGuid,
+          expectedPutData
+        ),
+        'PUT should be called with the expected API url and data'
+      );
 
       done();
     });
